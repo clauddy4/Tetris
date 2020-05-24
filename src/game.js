23c1eadad21be0d52db00ffd90348f3f4a352d7b
Olga Makarova <olgamakarovatvr@gmail.com>
2020-05-24 21:05:15 +0300

    change rotation method

--- src/game.js
+++ src/game.js
@@ -27,31 +27,10 @@ export default class Game {
     activePiece = {
         x: 0,
         y: 0,
-        get blocks() {
-            return this.rotations[this.rotationIndex];
-        },
-        rotationIndex: 0,
-        rotations: [
-            [
-                [0, 1, 0],
-                [1, 1, 1],
-                [0, 0, 0]
-            ],
-            [
-                [0, 1, 0],
-                [0, 1, 1],
-                [0, 1, 0]
-            ],
-            [
-                [0, 0, 0],
-                [1, 1, 1],
-                [0, 1, 0]
-            ],
-            [
-                [0, 1, 0],
-                [1, 1, 0],
-                [0, 1, 0]
-            ],
+        blocks: [
+            [0, 1, 0],
+            [1, 1, 1],
+            [0, 0, 0],
         ]
     };
 
@@ -74,14 +53,24 @@ export default class Game {
     }
 
     rotatePiece() {
-        this.activePiece.rotationIndex = this.activePiece.rotationIndex < 3 ? this.activePiece.rotationIndex + 1 : 0;
+        const blocks = this.activePiece.blocks;
+        const length = blocks.length;
 
-        console.log(this.hasCollision());
-        if (this.hasCollision()) {
-            this.activePiece.rotationIndex = this.activePiece.rotationIndex > 0 ? this.activePiece.rotationIndex - 1 : 3
+        const temp = [];
+        for (let i = 0; i < length; i++) {
+            temp[i] = new Array(length).fill(0);
+        }
+        for (let y = 0; y < length; y++) {
+            for (let x = 0; x < length; x++) {
+                temp[x][y] = blocks[length - 1 - y][x]
+            }
         }
 
-        return this.activePiece.blocks;
+        this.activePiece.blocks = temp;
+
+        if (this.hasCollision()) {
+            this.activePiece.blocks = blocks;
+        }
     }
 
     hasCollision() {
