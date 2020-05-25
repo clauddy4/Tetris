e6bcfe0ffa670684d135784f75429226b261d12d
Olga Makarova <olgamakarovatvr@gmail.com>
2020-05-25 12:50:29 +0300

    render

--- src/app.js
+++ src/app.js
@@ -1,5 +1,31 @@
 import Game from "./game.js";
+import View from "./view.js";
+
+const element = document.querySelector('#root');
 
 const game = new Game;
-window.game =game;
-console.log(game);
+const view = new View(root, 240, 480, 20, 10);
+
+window.game = game;
+window.view = view;
+
+document.addEventListener('keydown', event => {
+    switch (event.keyCode) {
+        case 37:
+            game.movePieceLeft();
+            view.render(game.getState());
+            break;
+        case 38:
+            game.rotatePiece();
+            view.render(game.getState());
+            break;
+        case 39:
+            game.movePieceRight();
+            view.render(game.getState());
+            break;
+        case 40:
+            game.movePieceDown();
+            view.render(game.getState());
+            break;
+    }
+});
