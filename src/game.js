5655586bb0ca3361398b4600a27e1b31e22232cc
Olga Makarova <olgamakarovatvr@gmail.com>
2020-05-25 15:32:39 +0300

    add score

--- src/game.js
+++ src/game.js
@@ -1,4 +1,11 @@
 export default class Game {
+    static points = {
+        '1': 40,
+        '2': 100,
+        '3': 300,
+        '4': 1200,
+    };
+
     score = 0;
     lines = 0;
     level = 0;
@@ -125,6 +132,8 @@ export default class Game {
         if (this.hasCollision()) {
             this.activePiece.y -= 1;
             this.lockPiece();
+            const clearedLines = this.clearLines();
+            this.updateScore(clearedLines);
             this.updatePieces();
         }
     }
@@ -185,6 +194,42 @@ export default class Game {
             for (let x = 0; x < blocks[y].length; x++) {
                 if (blocks[y][x]) this.playfield[pieceY + y][pieceX + x] = blocks[y][x];
             }
+        }
+    }
+
+    clearLines() {
+        const rows = 20;
+        const columns = 10;
+        let lines = [];
+
+        for (let y = rows - 1; y >= 0; y--) {
+            let numberOfBlocks = 0;
+            for (let x = 0; x < columns; x++) {
+                if (this.playfield[y][x]) {
+                    numberOfBlocks += 1;
+                }
+            }
+            if (numberOfBlocks === 0) {
+                break;
+            } else if (numberOfBlocks < columns) {
+                continue;
+            } else if (numberOfBlocks === columns) {
+                lines.unshift(y)
+            }
+        }
+        for (let index of lines) {
+            this.playfield.splice(index, 1);
+            this.playfield.unshift(new Array(columns).fill(0))
+        }
+
+        return lines.length;
+    }
+
+    updateScore(clearedLines) {
+        if (clearedLines > 0) {
+            this.score += Game.points[clearedLines];
+            this.lines += clearedLines;
+            console.log(this.score, this.lines)
         }
     }
 
