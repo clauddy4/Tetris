42cdf40f2f6d84ee1bd9f53ba1820f55b2264e7f
Olga Makarova <olgamakarovatvr@gmail.com>
2020-07-14 13:09:07 +0300

    refactor

--- src/view.js
+++ src/view.js
@@ -103,10 +103,8 @@ export default class View {
     }
 
     renderStartScreen() {
-        this.context.fillStyle = 'white';
-        this.context.font = '18px "Press Start 2P"';
-        this.context.textAlign = 'center';
-        this.context.textBaseline = 'middle';
+        
+        this.renderDefaultScreen();
         this.context.fillText('Press Enter to start', this.width / 2, this.height / 2);
     }
 
@@ -114,10 +112,7 @@ export default class View {
         this.context.fillStyle = 'rgba(0, 0, 0, 0.75)';
         this.context.fillRect(0, 0, this.width, this.height);
 
-        this.context.fillStyle = 'white';
-        this.context.font = '18px "Press Start 2P"';
-        this.context.textAlign = 'center';
-        this.context.textBaseline = 'middle';
+        this.renderDefaultScreen();
         this.context.fillText('Press ENTER to resume game', this.width / 2, this.height / 2);
     }
 
@@ -126,14 +121,18 @@ export default class View {
         this.context.fillStyle = 'rgba(0, 0, 0, 0.75)';
         this.context.fillRect(0, 0, this.width, this.height);
 
-        this.context.fillStyle = 'white';
-        this.context.font = '18px "Press Start 2P"';
-        this.context.textAlign = 'center';
-        this.context.textBaseline = 'middle';
+        this.renderDefaultScreen();
         this.context.fillText('GAME OVER', this.width / 2, this.height / 2 - 48);
         this.context.fillText(`Score: ${score}`, this.width / 2, this.height / 2);
         this.context.fillText('Press ENTER to restart', this.width / 2, this.height / 2 + 48 )
 
+    }
+
+    renderDefaultScreen() {
+        this.context.fillStyle = 'white';
+        this.context.font = '18px "Press Start 2P"';
+        this.context.textAlign = 'center';
+        this.context.textBaseline = 'middle';
     }
 
     clearScreen() {
